372e5995c6deec9866cc09711543b99e2efb4df5
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-07-16 15:48:38 -0400

    Aula 03

--- src/br/com/paulo/cliente/ClienteTarefas.java
+++ src/br/com/paulo/cliente/ClienteTarefas.java
@@ -1,5 +1,6 @@
 package br.com.paulo.cliente;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,16 +13,67 @@ public class ClienteTarefas {
 		
 		String clientName = Integer.toOctalString(socket.getLocalPort()).substring(4);
 		
-		PrintStream saida = new PrintStream(socket.getOutputStream());
-		saida.println("cliente : " + clientName);
+		Thread threadEnviarComando = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					System.out.println("*** Enviando comandos ao servidor ***");
+					PrintStream saida = new PrintStream(socket.getOutputStream());
+//					saida.println("cliente : " + clientName);
+					
+					System.out.print("> ");
+					Scanner teclado = new Scanner(System.in);	
+
+					while(teclado.hasNextLine()) {
+						String comando = teclado.nextLine();
+						
+						if (comando.trim().equals("")) {
+							break;
+						}
+						
+						saida.println(comando);
+						System.out.print("> ");
+					}
+					
+					saida.close();
+					teclado.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 		
-		Scanner teclado = new Scanner(System.in);
-		System.out.print("> ");
-		teclado.nextLine();
+		Thread threadReceberResposta = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("*** Recebendo dados do servidor ***");
+				Scanner respostaServidor;
+				try {
+					respostaServidor = new Scanner(socket.getInputStream());
+					
+					while(respostaServidor.hasNextLine()) {
+						String resposta = respostaServidor.nextLine();
+						System.out.println(resposta);
+					}
+					
+					respostaServidor.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				
+			}
+		});
 		
-		saida.close();
-		teclado.close();
+		threadReceberResposta.start();
+		threadEnviarComando.start();
+		
+		threadEnviarComando.join();
+		
+		System.out.println("*** Fechando socket do cliente ***");
 		socket.close();
+		
 		System.out.println("[ *** CONEXÃO ENCERRADA *** ]");
 	}
 
--- src/br/com/paulo/servidor/DistribuirTarefas.java
+++ src/br/com/paulo/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.paulo.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -18,11 +19,31 @@ public class DistribuirTarefas implements Runnable {
 			
 			Scanner entradaCliente = new Scanner(socket.getInputStream());
 			
+			PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+			
 			while(entradaCliente.hasNextLine()) {
 				String comando = entradaCliente.nextLine();
+				System.out.println("Comando recebido " + comando);					
+				
+				switch (comando) {
+				case "c1":
+					saidaCliente.println("Confirmação de comando c1");
+					break;
+				case "c2":
+					saidaCliente.println("Confirmação de comando c2");
+					break;
+				case "c3":
+					saidaCliente.println("Confirmação de comando c3");
+					break;
+				default:
+					saidaCliente.println("Comando inválido");
+					break;
+				}
+				
 				System.out.println(comando);
 			}
 			
+			saidaCliente.close();
 			entradaCliente.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
